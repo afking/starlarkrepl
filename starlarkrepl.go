c680246e84d6a3d74085975930017a4d67f5bd2f
Edward McFarlane <edward.mcfarlane@outlook.com>
2020-04-11 17:49:36 +0100

    basic completer

--- starlarkrepl.go
+++ starlarkrepl.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"io"
 	"sort"
+	"strconv"
 	"strings"
+	"unicode/utf8"
 
 	"github.com/peterh/liner"
 	"go.starlark.net/repl"
@@ -32,7 +34,8 @@ func Run(thread *starlark.Thread, globals starlark.StringDict) (err error) {
 }
 
 // finxPrefix assumes sorted arrays of keys
-func findPrefix(pfx string, keyss ...[]string) (c []string) {
+func findPrefix(line string, depth int, pfx string, keyss ...[]string) (c []string) {
+	pfx = strings.TrimSpace(pfx) // ignore any whitespacing
 	for _, keys := range keyss {
 		i := sort.SearchStrings(keys, pfx)
 		j := i
@@ -43,70 +46,214 @@ func findPrefix(pfx string, keyss ...[]string) (c []string) {
 		}
 		c = append(c, keys[i:j]...)
 	}
-	sort.Strings(c)
+	if len(keyss) > 1 {
+		sort.Strings(c)
+	}
+
+	// Add line start
+	for i := range c {
+		c[i] = line[:depth] + c[i]
+	}
 	return c
 }
 
-func newCompleter(globals starlark.StringDict) liner.Completer {
-	return func(line string) (c []string) {
-		if strings.Count(line, " ") == len(line) {
-			return []string{strings.Repeat(" ", (len(line)/4)*4+4)}
+// An Args is a starlark Callable with arguments.
+type Args interface {
+	starlark.Callable
+	ArgNames() []string
+}
+
+type completer struct {
+	globals starlark.StringDict
+}
+
+type typ int
+
+const (
+	unknown typ = iota - 1
+	root        //
+	dot         // .
+	brack       // []
+	paren       // ()
+	brace       // {}
+)
+
+func (t typ) String() string {
+	switch t {
+	case dot:
+		return "."
+	case brack:
+		return "["
+	case paren:
+		return "("
+	case brace:
+		return "{"
+	default:
+		return "?"
+	}
+}
+
+func enclosed(line string) (typ, int) {
+	k := len(line)
+	var parens, bracks, braces int
+	for size := 0; k > 0; k -= size {
+		var r rune
+		r, size = utf8.DecodeLastRuneInString(line[:k])
+		switch r {
+		case '(':
+			parens += 1
+		case ')':
+			parens -= 1
+		case '[':
+			bracks += 1
+		case ']':
+			bracks -= 1
+		case '{':
+			braces += 1
+		case '}':
+			braces -= 1
 		}
-		var wrote bool
-		f, err := syntax.ParseCompoundStmt("<stdin>", func() ([]byte, error) {
-			if wrote {
-				return nil, io.EOF
+		if parens > 0 {
+			return paren, k - size
+		}
+		if bracks > 0 {
+			return brack, k - size
+		}
+		if braces > 0 {
+			return brace, k - size
+		}
+	}
+	return unknown, -1
+}
+
+// complete tries to resolve a line variable to global named values.
+// TODO: use a proper parser to resolve values.
+func (c completer) complete(line string) (values []string) {
+	if strings.Count(line, " ") == len(line) {
+		// tab complete indent
+		return []string{strings.Repeat(" ", (len(line)/4)*4+4)}
+	}
+
+	type x struct {
+		typ   typ
+		value string
+		depth int
+	}
+
+	var xs []x
+
+	i := len(line)
+	j := i
+
+Loop:
+	for size := 0; i > 0; i -= size {
+		var r rune
+		switch r, size = utf8.DecodeLastRuneInString(line[:i]); r {
+		case '.': // attr
+			xs = append(xs, x{dot, line[i:j], i})
+		case '[': // index
+			xs = append(xs, x{brack, line[i:j], i})
+		case '(': // functions
+			xs = append(xs, x{paren, line[i:j], i})
+		case ' ', ',':
+			typ, k := enclosed(line[:i-size])
+
+			// Use ArgNames as possible completion
+			if typ == paren {
+				xs = append(xs, x{typ, line[i:j], i})
+				i, j = k, k
+				continue // loop
 			}
-			wrote = true
-			return []byte(line + "\n"), nil
-		})
-		if err != nil {
+
+			break Loop
+		case ';', '=', '{', '}':
+			break Loop // EOF
+		default:
+			continue // capture
+		}
+		j = i - size
+	}
+	xs = append(xs, x{root, line[i:j], i})
+
+	var cursor starlark.Value
+	for i := len(xs) - 1; i >= 0; i-- {
+		x := xs[i]
+
+		switch x.typ {
+		case root:
+			if i == 0 {
+				keys := [][]string{c.globals.Keys(), starlark.Universe.Keys()}
+				return findPrefix(line, x.depth, x.value, keys...)
+			}
+
+			if g := c.globals[x.value]; g != nil {
+				cursor = g
+			} else if u := starlark.Universe[x.value]; u != nil {
+				cursor = u
+			}
+		case dot:
+			v, ok := cursor.(starlark.HasAttrs)
+			if !ok {
+				return
+			}
+
+			if i == 0 {
+				return findPrefix(line, x.depth, x.value, v.AttrNames())
+			}
+
+			p, err := v.Attr(x.value)
+			if p == nil || err != nil {
+				return
+			}
+			cursor = p
+		case brack:
+			if i != 0 {
+				// TODO: resolve arg? fmt.Printf("TODO: resolve arg %s\n", x.value)
+				return
+			}
+
+			if strings.HasPrefix(x.value, "\"") {
+				v, ok := cursor.(starlark.IterableMapping)
+				if !ok {
+					return
+				}
+
+				iter := v.Iterate()
+				var keys []string
+				var p starlark.Value
+				for iter.Next(&p) {
+					s, ok := starlark.AsString(p)
+					if !ok {
+						continue // skip
+					}
+					keys = append(keys, strconv.Quote(s)+"]")
+				}
+				return findPrefix(line, x.depth, x.value, keys)
+			}
+			keys := [][]string{c.globals.Keys(), starlark.Universe.Keys()}
+			return findPrefix(line, x.depth, x.value, keys...)
+
+		case paren:
+			if i != 0 {
+				return // Functions aren't evalutated
+			}
+
+			keys := [][]string{c.globals.Keys(), starlark.Universe.Keys()}
+			v, ok := cursor.(Args)
+			if ok {
+				args := v.ArgNames()
+				for i := range args {
+					args[i] = args[i] + " = "
+				}
+				keys = append(keys, args)
+			}
+
+			return findPrefix(line, x.depth, x.value, keys...)
+		default:
 			return
 		}
-		fmt.Println(f, err)
-
-		if len(f.Stmts) == 0 {
-			return nil
-		}
-		var predeclared starlark.StringDict
-		if err := resolve.REPLChunk(f, globals.Has, predeclared.Has, starlark.Universe.Has); err != nil {
-			//fmt.Println("err", err)
-			//return []string{err.Error()}
-			// igore name pass failures
-		}
-		syntax.Walk(f.Stmts[0], func(n syntax.Node) bool {
-			if n == nil {
-				return false
-			}
-			start, end := n.Span()
-			fmt.Println(start.Col, end.Col)
-
-			switch n := n.(type) {
-			case *syntax.Ident:
-				fmt.Printf("%T\t%+v\n", n, n)
-				fmt.Println("\t---", n.Name, n.Binding)
-				keys := globals.Keys()
-				c = findPrefix(n.Name, keys, starlark.Universe.Keys())
-
-			case *syntax.DotExpr:
-				fmt.Printf("%T\t%+v\n", n, n)
-				fmt.Println("\t---", n.X)
-				fmt.Println("\t---", n.Name)
-
-			default:
-				fmt.Printf("%T\t%+v\n", n, n)
-			}
-			return true
-		})
-
-		//locals, err := resolve.Expr(f, globals.Has, starlark.Universe.Has)
-		//if err != nil {
-		//	fmt.Println("err", err)
-		//}
-		//fmt.Prinln("local", locals)
-
-		return
 	}
+	return
 }
 
 func suggest(line string) string {
@@ -148,7 +295,8 @@ func rep(line *liner.State, thread *starlark.Thread, globals starlark.StringDict
 		return []byte(s + "\n"), nil
 	}
 
-	//line.SetCompleter(newCompleter(globals))
+	c := completer{globals}
+	line.SetCompleter(c.complete)
 
 	f, err := syntax.ParseCompoundStmt("<stdin>", readline)
 	if err != nil {
--- starlarkrepl_test.go
+++ starlarkrepl_test.go
@@ -8,6 +8,21 @@ import (
 	"go.starlark.net/starlarkstruct"
 )
 
+type testCallableArgs struct {
+	args []string
+}
+
+func (c testCallableArgs) String() string        { return "callable_kwargs" }
+func (c testCallableArgs) Type() string          { return "callable_kwargs" }
+func (c testCallableArgs) Freeze()               {}
+func (c testCallableArgs) Truth() starlark.Bool  { return true }
+func (c testCallableArgs) Hash() (uint32, error) { return 0, nil }
+func (c testCallableArgs) Name() string          { return "callable_kwargs" }
+func (c testCallableArgs) CallInternal(thread *starlark.Thread, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	return starlark.None, nil
+}
+func (c testCallableArgs) ArgNames() []string { return c.args }
+
 func TestAutoComplete(t *testing.T) {
 	mod := &starlarkstruct.Module{
 		Name: "hello",
@@ -18,6 +33,9 @@ func TestAutoComplete(t *testing.T) {
 				d.SetKey(starlark.String("key"), starlark.String("value"))
 				return d
 			}(),
+			"func": testCallableArgs{[]string{
+				"kwarg",
+			}},
 		},
 	}
 
@@ -34,23 +52,74 @@ func TestAutoComplete(t *testing.T) {
 		line: "a",
 		want: []string{"abc", "all", "any"},
 	}, {
+		name: "simple_semi",
+		globals: map[string]starlark.Value{
+			"abc": starlark.String("hello"),
+		},
+		line: "abc = \"hello\"; a",
+		want: []string{
+			"abc = \"hello\"; abc",
+			"abc = \"hello\"; all",
+			"abc = \"hello\"; any",
+		},
+	}, {
+		name: "assignment",
+		globals: map[string]starlark.Value{
+			"abc": starlark.String("hello"),
+		},
+		line: "abc = a",
+		want: []string{
+			"abc = abc",
+			"abc = all",
+			"abc = any",
+		},
+	}, {
 		name: "nest",
 		globals: map[string]starlark.Value{
 			"hello": mod,
 		},
 		line: "hello.wo",
 		want: []string{"hello.world"},
-		//}, {
-		//	name: "dict",
-		//	globals: map[string]starlark.Value{
-		//		"hello": mod,
-		//	},
-		//	line: "hello.dict[\"",
-		//	want: []string{"hello.dict[\"key\"]"},
+	}, {
+		name: "dict",
+		globals: map[string]starlark.Value{
+			"abc":   starlark.String("hello"),
+			"hello": mod,
+		},
+		line: "hello.dict[ab",
+		want: []string{"hello.dict[abc"},
+	}, {
+		name: "dict_string",
+		globals: map[string]starlark.Value{
+			"hello": mod,
+		},
+		line: "hello.dict[\"",
+		want: []string{"hello.dict[\"key\"]"},
+	}, {
+		name: "call",
+		globals: map[string]starlark.Value{
+			"func": testCallableArgs{[]string{
+				"arg_one", "arg_two",
+			}},
+		},
+		line: "func(arg_",
+		want: []string{"func(arg_one = ", "func(arg_two = "},
+	}, {
+		name: "call_multi",
+		globals: map[string]starlark.Value{
+			"func": testCallableArgs{[]string{
+				"arg_one", "arg_two",
+			}},
+		},
+		line: "func(arg_one = func(), arg_",
+		want: []string{
+			"func(arg_one = func(), arg_one = ",
+			"func(arg_one = func(), arg_two = ",
+		},
 	}} {
 		t.Run(tt.name, func(t *testing.T) {
-			cmplter := newCompleter(tt.globals)
-			got := cmplter(tt.line)
+			c := completer{tt.globals}
+			got := c.complete(tt.line)
 
 			if !reflect.DeepEqual(tt.want, got) {
 				t.Errorf("%v != %v", tt.want, got)
